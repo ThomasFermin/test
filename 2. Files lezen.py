a803133c090726a312c1ce8b20b81e60acbcb8e2
Thomas Fermin <tfer97@hotmail.nl>
2018-10-01 12:08:45 +0200

    First commit

---
+++ 2. Files lezen.py
@@ -0,0 +1,3 @@
+infile=open("kaartnummers.txt","w")
+infile.write("325255, Jan Jansen\n334343, Erik Materus\n235434, Ali Ahson\n645345, Eva Versteeg\n534545, Jan de Wilde\n345355, Henk de Vries")
+infile.close()
