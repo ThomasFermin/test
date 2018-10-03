358882fc2761345923e00ca265af1f2246e28f46
Thomas Fermin <tfer97@hotmail.nl>
2018-10-03 21:19:34 +0200

    First commit

---
+++ 5. Nested loop.py
@@ -0,0 +1,8 @@
+    def tables():
+        for x in range (1, 11):
+            print("Table for {}".format(x))
+            for y in range (1, 11):
+                print(x * y, end=" ")
+            print()
+
+    tables()
